b217fe70211743bdbcb0dca876af7ff131615dc7
ahmetkara-hub <ahmetkara144@gmail.com>
2026-07-11 17:20:32 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.title("First NumPy + Matplotlib Test")
+plt.grid(True)
+plt.show()
